830b252593dd18d5234f779e7862bc2c1572233b
ELITE-E <eliteeric0468@gmail.com>
2025-05-06 20:04:13 +0300

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: true, title: "shop" }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: true, title: "categories" }}
+      />
+      <Stack.Screen
+        name="products"
+        options={{ headerShown: true, title: "products" }}
+      />
+      <Stack.Screen
+        name="auth"
+        options={{ headerShown: true, title: "auth" }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{ presentation: "modal", title: "cart" }}
+      />
+    </Stack>
+  );
+}
