c921b4cc159ffc1982127e304bf82ff5041af37f
ELITE-E <eliteeric0468@gmail.com>
2025-05-19 20:44:10 +0300

    listheader

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,17 +1,25 @@
 import { FlatList, StyleSheet, View, Text } from "react-native";
 // import { PRODUCTS } from "../../../appassets/products";
 import { PRODUCTS } from "../../../assets/products";
+import { ProductListItem } from "../components/product-list-item";
+import { ListHeader } from "../components/list-header";
 import React from "react";
 
 const Home = () => {
   return (
     <View>
+      {/* <ListHeader /> */}
+      {/* <View style={{ padding: 10 }} /> */}
       <FlatList
         data={PRODUCTS}
-        renderItem={({ item }) => <View></View>}
+        renderItem={({ item }) => (
+          <View>
+            <ProductListItem product={item} />
+          </View>
+        )}
         keyExtractor={(item) => item.id.toString()}
         numColumns={2}
-        ListHeaderComponent={<Text>Products</Text>}
+        ListHeaderComponent={ListHeader}
         contentContainerStyle={styles.FlatListContent}
         columnWrapperStyle={styles.FlatListColumn}
         style={{
